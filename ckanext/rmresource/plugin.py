eebb1e10fa8b9921e5760bf13f9e52cf4ace7e56
dassolkim <kimds0926@gmail.com>
2019-06-13 14:46:37 +0900

    add remove function

--- ckanext/rmresource/plugin.py
+++ ckanext/rmresource/plugin.py
@@ -13,14 +13,16 @@ log = logging.getLogger(__name__)
 def dsfile_remove(id):
 
     # id = data_dict.get('id')
-    url = 'http://114.70.235.44:65000/api/action/datastore_delete'
-    headers = {'Content-Type': 'application/json', 'Authorization': 'f91cafff-3f29-4e03-8dfb-ba30d74b4c81'}
+
+    # your ckan host url + apiname
+
+    url = 'ckan_host_url'
+
+    # fill in your platform information
+    headers = {'Content-Type': 'application/json', 'Authorization': 'apikey'}
     data = {'resource_id': id, 'force': True}
     response = requests.post(url=url, headers=headers, data=json.dumps(data))
-    print('################################## finish delete resource in datastore ##########################')
-    print(response)
-    print(response.json())
-    log.debug('Delete {0} resource in Filestore'.format(id))
+    log.debug('Delete {0} resource in Datastore'.format(id))
 
 
 def file_remove(resource_id):
@@ -29,16 +31,14 @@ def file_remove(resource_id):
     storage_path = uploader.get_storage_path()
     directory = os.path.join(storage_path, 'resources', resource_id[0:3], resource_id[3:6])
     filepath = os.path.join(directory, resource_id[6:])
-    print('################################## finish delete resource in filestore ##########################')
-    print(directory)
-    print(filepath)
+
     try:
         os.remove(filepath)
-        print('remove filepath {0}').format(filepath)
+        log.info('remove filepath {0}'.format(filepath))
         os.removedirs(directory)
-        print('remove directory {0}').format(directory)
-        log.info(u'Resource file in %s has been deleted.' % filepath)
-        log.debug('Delete {0} resource in Datastore'.format(resource_id))
+        log.info('remove directory {0}'.format(directory))
+        log.debug('Delete {0} resource in Filestore'.format(resource_id))
+
     except OSError, e:
         log.debug(u'Error: %s - %s.' % (e.filename, e.strerror))
         pass
